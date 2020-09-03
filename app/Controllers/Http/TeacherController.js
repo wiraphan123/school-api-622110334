24c18dc0cdf33172f5b3c5bd19deb09c225891ab
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-03 23:00:19 +0700

    edit/Controllers/Http-TeacherController

--- app/Controllers/Http/TeacherController.js
+++ app/Controllers/Http/TeacherController.js
@@ -1,23 +1,29 @@
 'use strict'
 
 const Database = use('Database')
-function numberTypeParamValidator(number){
-    if(Number.isNaN(parseInt(number))) 
-    return { error:`param: ${number} is not supported, Please use number type param.` }
-        return {}
-}
-
+const Teacher = use('App/Models/Teacher')
+const Validator = use('Validator')
+// function numberTypeParamValidator(number){
+//     if(Number.isNaN(parseInt(number))) 
+//     return { error:`param: ${number} is not supported, Please use number type param.` }
+//         return {}
+// }
 const Hash = use('Hash')
 
 class TeacherController {
     async index(){
-        const teachers = await Database.table('teachers')
-        return {status:200,error:undefined,data:teachers}
+        const { references } = request.qs
+        const teachers = Teacher.query()
+        if(references){
+            const extractReferences = references.split(",")
+            subject.with(extractReferences)
+        }
+        return { status:200,error:undefined,data:await teachers.fetch() }
     }
     async show({ request }){
         const { id } = request.params
         
-        const ValidatedValue = numberTypeParamValidator(id)
+        const ValidatedValue = Validator(id)
         
         if(ValidatedValue.error) return {status:500, error:ValidatedValue.error ,data:undefined}
 
@@ -34,15 +40,16 @@ class TeacherController {
 
     async store({ request }){
         const { first_name, last_name, email, password } = request.body
-        
-        const missingKeys = []
-        if (!first_name) missingKeys.push('first_name')
-        if (!last_name) missingKeys.push('last_name')
-        if (!email) missingKeys.push('email')
-        if (!password) missingKeys.push('password')
-
-        if (missingKeys.length)
-        return {status: 422, error: `${missingKeys} is missing`, data: undefined}
+       
+        const rules = {
+            first_name:'required',
+            last_name: 'required',
+            email:'required|email|unique:teachers.email',
+            password:'required|min:8'
+        }
+        const validation = await Validator.validateAll(request.body,rules)
+        if(validation.fails())
+        return{ status:422,error:validation.messages(),data:undefined }
 
         const hashedPassword = await Hash.make(password)
         const teacher = await Database
