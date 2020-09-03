6348be98b5774cf35c1136f55c6e5e6cc368740f
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-03 22:59:00 +0700

    edit/Controllers/Http-SubjectController

--- app/Controllers/Http/SubjectController.js
+++ app/Controllers/Http/SubjectController.js
@@ -1,48 +1,87 @@
 'use strict'
+
 const Database = use('Database')
-function numberTypeParamValidator(number){
-    if(Number.isNaN(parseInt(number))) 
-    return { error:`param: ${number} is not supported, Please use number type param.` }
-        return {}
-}
+const Subject = use('App/Models/Subject')
+const Validator = use('Validator')
 class SubjectController {
-    async index(){
-        const subjects = await Database.table('subjects')
-        return {status:200,error:undefined,data:subjects}
+//-----------------------------------------------------------------------------------------------------------------------------------
+    async index({ request }){
+        // subjects?references=teachers
+        const { references } = request.qs
+        console.log(references)
+
+        const subjects = Subject.query()
+        if(references){
+            const extractReferences = references.split(",")
+            subject.with(extractReferences)
+        }
+        return { status:200,error:undefined,data:await subjects.fetch() }
     }
+    //-------------------------------------------------------------------------------------------------------------------------------------
     async show({ request }){
         const { id } = request.params
-        
-        const ValidatedValue = numberTypeParamValidator(id)
-        
+        const ValidatedValue = Validator(id)
         if(ValidatedValue.error) return {status:500, error:ValidatedValue.error ,data:undefined}
-
-        const subjects = await Database.select('*')
-         
-        .from('teachers')
-        .where("subject_id",id)
-        .first()
-        console.log(parseInt(id))
-       
-
+        const subject = await Subject.find(id)
         return {status:200,data: subjects || {} }
     }
+    
+    async showTeacher({ request }){
+        const { id } = request.params
+        const subject = await Database
+        .table('subject')
+        .where({subject_id: id})
+        .innerJoin('teachers','subject.teachers_id','teachers.teacher_id')
+        .first()
+
+        return { status:200,error:undefined,data:subject || {} }
+    }
+
+
     async store({ request }){
         const { title,teacher_id } = request.body
+        const rules = {
+            title:'required',
+            teacher_id: 'required',
+        }
+        const validation = await Validator.validateAll(request.body,rules)
+        if(validation.fails())
+        return{ status:422,error:validation.messages(),data:undefined }
         
-        const missingKeys = []
-        if (!title) missingKeys.push('title')
-        if (!teacher_id) missingKeys.push('teacher_id')
-        if (missingKeys.length)
-        return {status: 422, error: `${missingKeys} is missing`, data: undefined}
-        
-        const subjects = await Database
-        .table('subjects')
-        .insert({ title,teacher_id })
+        const subject = await Database
+            .table('subjects')
+            .insert({ title,teacher_id })
 
 
-    return { status:200,error:undefined,data:{ title,teacher_id } }
+        return { status:200,error:undefined,data:{ title,teacher_id } }
     }
+        
+    async update({ request }){
+        const { body,params } = request
+        const { id } = params
+        const { title,teacher_id } = body
+    
+        const subjectID = await Database
+        .table('subjects')
+        .where({ subject_id:id })
+        .update({ title,teacher_id })
+    
+        const subject = await Database
+        .table('subjects')
+        .where({ subject_id:subjectID })
+        .first()
+    
+        return { status:200,error:undefined,data:subjectID }
+        }
+        async destroy( { request } ){
+            const  {id} = request.params
+            const deleteSubject = await Database
+            .table('subjects').where({ subject_id:id })
+            .delete()
+            return { status:200, error:undefined, message:'success' } 
+    
+        }
+
 }
 
 module.exports = SubjectController
