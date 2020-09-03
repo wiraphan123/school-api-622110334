c00da6c5137ba3e426e33d68bcd3171c98d7f102
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-03 22:55:55 +0700

    edit/Controllers/Http-EnrollmentController

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -1,19 +1,23 @@
 'use strict'
+
 const Database = use('Database')
-function numberTypeParamValidator(number){
-    if(Number.isNaN(parseInt(number))) 
-    return { error:`param: ${number} is not supported, Please use number type param.` }
-        return {}
-    }
+const Validator = use('Validator')
+const enrollment = use('App/Models/Enrollment')
 class EnrollmentController {
+
     async index(){
-        const enrollments = await Database.table('enrollments')
-        return {status:200,error:undefined,data:enrollments}
+        const { references } = request.qs
+        const enrollments = enrollment.query()
+        if(references){
+            const extractReferences = references.split(",")
+            enrollment.with(extractReferences)
+        }
+        return { status:200,error:undefined,data:await enrollments.fetch() }
     }
     async show({ request }){
         const { id } = request.params
         
-        const ValidatedValue = numberTypeParamValidator(id)
+        const ValidatedValue = Validator(id)
         
         if(ValidatedValue.error) return {status:500, error:ValidatedValue.error ,data:undefined}
 
@@ -30,22 +34,49 @@ class EnrollmentController {
     async store({ request }){
         const { mark,mark_date,student_id,subject_id } = request.body
         
-        const missingKeys = []
-        if (!mark) missingKeys.push('mark')
-        if (!mark_date) missingKeys.push('mark_date')
-        if (!student_id) missingKeys.push('student_id')
-        if (!subject_id) missingKeys.push('subject_id')
-        if (missingKeys.length)
-        return {status: 422, error: `${missingKeys} is missing`, data: undefined}
-        
-        const subjects = await Database
-        .table('subjects')
+        const rules = {
+            mark:'required',
+            mark_date: 'required',
+            student_id:'required',
+            subject_id:'required'
+        }
+        const validation = await Validator.validateAll(request.body,rules)
+        if(validation.fails())
+        return{ status:422,error:validation.messages(),data:undefined }
+
+        const enrollments = await Database
+        .table('enrollments')
         .insert({  mark,mark_date,student_id,subject_id })
 
 
     return { status:200,error:undefined,data:{  mark,mark_date,student_id,subject_id } }
     }
-
+    async update({ request }){
+        const { body,params } = request
+        const { id } = params
+        const { mark,mark_date,student_id,subject_id } = body
+    
+        const enrollmentID = await Database
+        .table('enrollments')
+        .where({ enrollment_id:id })
+        .update({ mark,mark_date,student_id,subject_id })
+    
+        const enrollment = await Database
+        .table('groups')
+        .where({ enrollment_id:enrollmentID })
+        .first()
+    
+        return { status:200,error:undefined,data:enrollmentID }
+        }
+        async destroy( { request } ){
+            const  {id} = request.params
+            const deleteEnrollment = await Database
+            .table('enrollments').where({ enrollment_id:id })
+            .delete()
+            
+            return { status:200, error:undefined, message:'success' } 
+    
+        }
 
 }
 
